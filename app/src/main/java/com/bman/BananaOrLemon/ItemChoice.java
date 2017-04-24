5fc6399cb94ad968ffbde9570e6646ef8b1d555b
Omar Samara <omsamara@ncsu.edu>
2017-04-24 16:04:19 -0400

    Added in ItemChoice class

---
+++ app/src/main/java/com/bman/BananaOrLemon/ItemChoice.java
@@ -0,0 +1,15 @@
+package com.bman.BananaOrLemon;
+
+/**
+ * A class representing the image that is displayed to the user.
+ * The image will either be a lemon or a banana.
+ * @author Omar Samara
+ */
+public class ItemChoice {
+
+    private int isLemon;
+
+    public ItemChoice(boolean isLemon) {
+
+    }
+}
